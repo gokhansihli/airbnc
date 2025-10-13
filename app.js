8bd6d16c9bf2ca973f076c03a5b7f8c27423c9fd
Gökhan Sihli <gokhansihli@Mac.lan>
2025-10-13 19:33:14 +0100

    Fixed express static

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRouter = require("./routes/api-router");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public/index")));
 
 app.use(express.json());
 
